d4618871c081c9c75e387c3d88666e6a494616b2
kyreenn <157349892+kyreenn@users.noreply.github.com>
2024-01-26 09:33:07 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment 
+ *
+ *  Author:     Kyreen Johnson
+ *  Course:     COMP-003A-L01
+ *  Purpose:    Code for Assignment 1
+ */
+namespace COMP003A.Assignment1;
+
+    internal class Program
+    {  
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 things about me");
+        Console.WriteLine("1. I am 24 years old");
+        Console.WriteLine("2. My birthday is June 2nd");
+        Console.WriteLine("3. I am a gemini");
+        Console.WriteLine("4. I live with my mom");
+        Console.WriteLine("5. I have no kids");
+        Console.WriteLine("6. I dont like pickles");
+        Console.WriteLine("7. I play games");
+        Console.WriteLine("8. I have an iPhone");
+        Console.WriteLine("9. I like Lofi Music");
+        Console.WriteLine("10. I dont like cats");
+        Console.WriteLine("11. Im taking 6 classes");
+        Console.WriteLine("12. I like to sleep");
+        Console.WriteLine("13. I Have a dog");
+        Console.WriteLine("14. I have one older brother and two younger sisters");
+        Console.WriteLine("15. I have curly hair");
+        Console.WriteLine("16. I build computers");
+        Console.WriteLine("17. I work at the casino as a slot technican");
+        Console.WriteLine("18. I am single");
+        Console.WriteLine("19. I have a big family");
+        Console.WriteLine("20. I have a backback");
+        Console.WriteLine("21. I dont like socializing");
+        Console.WriteLine("22. I wanna transfer to fresno state");
+        Console.WriteLine("23. I wear glasses");
+        Console.WriteLine("24. My football team is steelers");
+        Console.WriteLine("25. I hardly know how to code");
+        Console.WriteLine("26. I had braces");
+        Console.WriteLine("27. My ethnecity is black, mexican, and japanese");
+        Console.WriteLine("28. I live here in lemoore");
+        Console.WriteLine("29. I work at a casino");
+        Console.WriteLine("30. I have a toyota camry");
+        Console.WriteLine("31. The Kid Laroi is one of my favorite artist to listen to");
+        Console.WriteLine("32. I own everything apple");
+        Console.WriteLine("33. I work with a bunch of old people");
+        Console.WriteLine("34. I like to go out to bars");
+        Console.WriteLine("35. I like to code");
+        Console.WriteLine("36. Being around a lot of people drains my social battery");
+        Console.WriteLine("37. I am very understanding and kind to others");
+        Console.WriteLine("38. I dont think im smart or very good at anything but I wont give up until i figure it out");
+        Console.WriteLine("39. I like to ask questions");
+        Console.WriteLine("40. I've had a total of 5 girlfriends");
+        Console.WriteLine("41. I dont like to smoke");
+        Console.WriteLine("42. I am still really good friends with my childhood friend");
+        Console.WriteLine("43. I am 5'10");
+        Console.WriteLine("44. my weight is 135lbs");
+        Console.WriteLine("45. I have straight teeth but growing up they were not straight");
+        Console.WriteLine("46. I have gotten into a car accident");
+        Console.WriteLine("47. I have brown eyes");
+        Console.WriteLine("48. I have never broken a bone before");
+        Console.WriteLine("49. I never been stung by a bee");
+        Console.WriteLine("50. I am usually a quiet person");
+        Console.WriteLine("51. I wear glasses");
+        Console.WriteLine("52. I shower twice a day");
+        Console.WriteLine("53. I brush my teeth multiple times a day because im bored");
+        Console.WriteLine("54. I like to play tennis and baseball");
+        Console.WriteLine("55. My favroite animals are turtles/pandas");
+        Console.WriteLine("56. My favorite season is winter");
+        Console.WriteLine("57. I hate the heat");
+        Console.WriteLine("58. I wanna vist Japan");
+        Console.WriteLine("59. I can play the ocarina");
+        Console.WriteLine("60. My first job was taco bell");
+        Console.WriteLine("61. I am afriad of failure");
+        Console.WriteLine("62. My celebrity crush is valkyrae");
+        Console.WriteLine("63. I have no piercings");
+        Console.WriteLine("64. I have 5 tattoos");
+        Console.WriteLine("65. My favorite word is noodles");
+        Console.WriteLine("66. If I could have a super power, it would be to fly");
+        Console.WriteLine("67. I was an essential worker when convid happened");
+        Console.WriteLine("68. I got my license when I was 19");
+        Console.WriteLine("69. I've been in my first physical fight at a bar and won");
+        Console.WriteLine("70. I have never been married");
+        Console.WriteLine("71. I go to the gym twince a week");
+        Console.WriteLine("72. I try to spend time with all my family");
+        Console.WriteLine("73. I like to watch youtube videos about how it is being a software developer or cyber security");
+        Console.WriteLine("74. I like action movies");
+        Console.WriteLine("75. I like all women");
+        Console.WriteLine("76. I only speak english");
+        Console.WriteLine("77. I have an ipad");
+        Console.WriteLine("78. I played soccor");
+        Console.WriteLine("79. I love to go on late night drives");
+        Console.WriteLine("80. I like to read");
+        Console.WriteLine("81. I love sad music");
+        Console.WriteLine("82. I would rather sit by myself and work by myself instead of working in groups");
+        Console.WriteLine("83. I dont like seafood");
+        Console.WriteLine("84. I love to go to six flags to ride the rollercosters");
+        Console.WriteLine("85. I dont like to swim");
+        Console.WriteLine("86. I dont like to eat popcorn");
+        Console.WriteLine("87. I dont like ice cream");
+        Console.WriteLine("88. I dont have a sweet tooth");
+        Console.WriteLine("89. I Have a terrible memory");
+        Console.WriteLine("90. I went to a highschool called hanford west high school");
+        Console.WriteLine("91. I got kicked out of my middle school and got sent to crossroads");
+        Console.WriteLine("92. i never went anywhere outside of california");
+        Console.WriteLine("93. I like to go go-karting");
+        Console.WriteLine("94. I love to go to places like dave and busters and adventure park");
+        Console.WriteLine("95. I am not a fan of zoo's");
+        Console.WriteLine("96. I grew up always going to church on sundays");
+        Console.WriteLine("87. I love to stream the games I play");
+        Console.WriteLine("98. I like to spend money and have a hard time saving it");
+        Console.WriteLine("99. I like to spend time star gazing");
+        Console.WriteLine("100. I love going to beach");
+
+    }
+}
